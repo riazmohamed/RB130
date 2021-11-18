010b5be7d97e40bc7542e3681f0b8d3c0317f9f7
riazmohamed <railsdevriaz@gmail.com>
2021-11-18 04:22:30 +0000

    creating a custom reduce method

---
+++ lesson1/build_reduce_method.rb
@@ -0,0 +1,12 @@
+def reduce(array, starting_value=0)
+  array.each do |num|
+    starting_value = yield(starting_value, num)
+  end
+  starting_value
+end
+
+array = [1, 2, 3, 4, 5]
+
+p reduce(array) { |acc, num| acc + num }                    # => 15
+p reduce(array, 10) { |acc, num| acc + num }                # => 25
+p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
--- lesson1/notes.md
+++ lesson1/notes.md
@@ -201,5 +201,3 @@ def select(array)
 end
 ```
 
-
-
