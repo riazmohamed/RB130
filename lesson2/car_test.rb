fabe2fb64d8cf73d869df06d42d814d7c8a3dcbf
riazmohamed <railsdevriaz@gmail.com>
2021-11-23 11:32:37 +0000

    Adding minitest reporters and skip keywords

--- lesson2/car_test.rb
+++ lesson2/car_test.rb
@@ -1,4 +1,6 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 require_relative 'car'
 
@@ -6,5 +8,11 @@ class CarTest < Minitest::Test
   def test_wheels
     car = Car.new
     assert_equal(4, car.wheels)
+  end
+
+  def test_bad_wheels
+    skip
+    car = Car.new
+    assert_equal(3, car.wheels)
   end
 end
