9e104e1a4ee9329819de720c9bc9fc5a73cd2701
riazmohamed <railsdevriaz@gmail.com>
2021-12-10 09:43:54 +0000

    Update PEDAC

--- ruby_challenges/triangles.rb
+++ ruby_challenges/triangles.rb
@@ -42,7 +42,7 @@ The following test cases should produce an error as
     Triangle.new(3, 4, -5)
 
 - The sum of any two sides are not greater than the third side
-    Triangle.new(7, 3, 2)
+    Triangle.new(7, 3, 2) ---> 7 + 2 , 7 + 3,
     Triangle.new(10, 1, 3)
     Triangle.new(1, 1, 2)
 
@@ -69,19 +69,19 @@ Output: string
     - otherwise return "scalene"
 
   - define #all_sides_equal - check if all the sides are equal
-    - @side1 == @side2 && @side2 == @side3
+    - side1 == side2 && side2 == side3
 
   - define #greater_than_zero - check if all the sides are greater then zero
-    [@side1, @side2, @side3].all? { |num| num > 0 }
+    [side1, side2, side3].all? { |num| num > 0 }
 
   - define #two_sides_greater - check if any two sides are greater than the third
-    @side1 + @side2 > @side3 ||
-    @side2 + @side3 > @side1 ||
-    @side1 + @side3 > @side 2
+    side1 + side2 > side3 &&
+    side2 + side3 > side1 &&
+    side1 + side3 > side 2
 
   - define two_sides_equal
     any two sides are equal
-    - @side1 == @side2 || @side2 == @side3
+    - [side1, side2, side3].uniq.size == 2
 
   - valid? - private helper method
     - checks if the triangle is valid
@@ -91,13 +91,13 @@ Output: string
 # Code With Intent
 
 class Triangle
-  attr_reader :kind
+attr_accessor :side1, :side2, :side3
 
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    raise ArgumentError.new("This is not a valid triangle!") unless valid?
+    raise ArgumentError.new("Invalid triangle lengths") unless valid?
   end
 
   def kind
@@ -105,32 +105,32 @@ class Triangle
       "equilateral"
     elsif two_sides_equal
       "isosceles"
-    else
+    elsif two_sides_greater
       "scalene"
     end
   end
 
   def all_sides_equal
-    [@side1, @side2].all?(@side3)
+    [side1, side2].all?(side3)
   end
 
   def greater_than_zero
-    [@side1, @side2, @side3].all? { |num| num > 0 }
+    [side1, side2, side3].all? { |num| num > 0 }
   end
 
   def two_sides_greater
-    @side1 + @side2 > @side3 ||
-    @side2 + @side3 > @side1 ||
-    @side1 + @side3 > @side2
+    side1 + side2 > side3 &&
+    side2 + side3 > side1 &&
+    side1 + side3 > side2
   end
 
   def two_sides_equal
-    @side1 == @side2 || @side2 == @side3
+    [side1, side2, side3].uniq.size == 2
   end
 
   private
 
   def valid?
-    all_sides_equal || greater_than_zero || two_sides_greater
+    greater_than_zero && two_sides_greater
   end
 end
