10121b090e4a3c030c91bd4da5bbef30fb8448c2
riazmohamed <railsdevriaz@gmail.com>
2021-12-10 07:45:47 +0000

    add test cases

---
+++ ruby_challenges/triangles.rb
@@ -0,0 +1,40 @@
+# Write a program to determine whether a triangle is equilateral, isosceles, or scalene.
+
+
+=begin
+========== THE PEDAC PROCESS ==========
+
+=====> Problem:
+Input: Given a triangle
+        # An equilateral triangle has all three sides the same length.
+        # An isosceles triangle has exactly two sides of the same length.
+        # A scalene triangle has all sides of different lengths.
+
+Output: A string which says whether a triangle is equilateral, isosceles, or scalene.
+
+=====> Rules
+Explicit Requiremnts:
+  - given length of the sides of the triangle as integers
+  -
+Implicit Requiremnts:
+  - the sides of the triangle has to be greater than zero
+  - 
+
+=====> (Any Questions / Assumptions needing clarification?)
+(e.g. return the same string object or an entirely new string???)
+
+=====> Examples, Edge Cases(test rules and boundaries)
+
+
+=====> Data structure(s):
+Input:
+Intermediate:
+Output:
+
+=====> Algorithm:
+
+=====> Implementation (Optional):
+
+=end
+
+# Code With Intent
