d8df27d8ee6d1acfb31f4f9c5d7e3d1fdaa2ffff
riazmohamed <railsdevriaz@gmail.com>
2022-01-09 22:03:27 +0000

    series.rb

---
+++ ruby_challenges/series.rb
@@ -0,0 +1,13 @@
+class Series
+  attr_reader :num
+  def initialize(num)
+    @num = num
+  end
+
+  def slices(number)
+    raise ArgumentError.new unless num.size >= number
+    arr = []
+    num.chars.each_cons(number) { |ele| arr << ele.map(&:to_i) }
+    arr
+  end
+end
