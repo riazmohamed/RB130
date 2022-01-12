cd2877e09febf8288f920e27281648e374709991
riazmohamed <railsdevriaz@gmail.com>
2022-01-12 22:24:19 +0000

    rubocop the code

--- ruby_challenges/octal.rb
+++ ruby_challenges/octal.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Style/FrozenStringLiteralComment
+# rubocop:disable  Style/Documentation
 class Octal
   attr_reader :num
 
@@ -6,13 +8,16 @@ class Octal
   end
 
   def valid_octal_number?
-    num.chars.all? { |ele| ("0".."7").to_a.include? ele }
+    num.chars.all? { |ele| ('0'..'7').to_a.include? ele }
   end
 
   def to_decimal
     return 0 unless valid_octal_number?
+
     num.to_i.digits.map.with_index { |n, i| n * 8**i }.sum
   end
 
   private :valid_octal_number?
 end
+# rubocop:enable Style/FrozenStringLiteralComment
+# rubocop:enable  Style/Documentation
