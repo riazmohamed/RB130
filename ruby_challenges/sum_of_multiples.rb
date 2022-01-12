b7bc4eeba1d938dfa6daa821b9499cfde56f6da9
riazmohamed <railsdevriaz@gmail.com>
2022-01-12 23:52:16 +0000

    Rubocop the final answer

--- ruby_challenges/sum_of_multiples.rb
+++ ruby_challenges/sum_of_multiples.rb
@@ -1,15 +1,19 @@
+# rubocop:disable Style/FrozenStringLiteralComment
+# rubocop:disable  Style/Documentation
 class SumOfMultiples
   attr_reader :value
 
- def initialize(*value)
-   @value = value == [] ? [3, 5] : value
- end
+  def initialize(*value)
+    @value = value == [] ? [3, 5] : value
+  end
 
- def self.to(num)
-   SumOfMultiples.new().to(num)
- end
+  def self.to(num)
+    SumOfMultiples.new.to(num)
+  end
 
- def to(num)
-   (1...num).select { |n| value.any? { |mul| n % mul == 0 } }.sum
- end
+  def to(num)
+    (1...num).select { |n| value.any? { |mul| (n % mul).zero? } }.sum
+  end
 end
+# rubocop:enable Style/FrozenStringLiteralComment
+# rubocop:enable  Style/Documentation
